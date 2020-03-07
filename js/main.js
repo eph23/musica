3cae2e277ee043b436f02bf7cab662da480fba6d
ephraim <ephraims.818@gmail.com>
2020-03-08 01:18:23 +0600

    Bio and Carousel added

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-$('.hero-carousel').owlCarousel({
+// $('.hero-carousel').owlCarousel({
+//   loop: true,
+//   dots: false,
+//   autoplay: true,
+//   autoplayTimeout: 3000,
+//   margin: 0,
+//   nav: false,
+//   touchDrag: true,
+//   mouseDrag: false,
+//   responsive: {
+//     0: {
+//       items: 1
+//     },
+//     600: {
+//       items: 1
+//     },
+//     1000: {
+//       items: 1
+//     }
+//   }
+// })
+$('.music-carousel').owlCarousel({
   loop: true,
   dots: false,
   autoplay: true,
   autoplayTimeout: 3000,
-  margin: 0,
-  nav: false,
-  touchDrag: true,
-  mouseDrag: false,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 1
-    },
-    1000: {
-      items: 1
-    }
-  }
-})
-$('.carousel-testimonial').owlCarousel({
-  loop: true,
-  dots: false,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  margin: 0,
+  margin: 40,
   nav: false,
   responsive: {
     0: {
