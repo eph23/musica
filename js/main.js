38706e07ed34ca1d629937b348848759eca2493c
ephraim <ephraims.818@gmail.com>
2020-03-08 20:34:54 +0600

    Alll sections added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-// $('.hero-carousel').owlCarousel({
-//   loop: true,
-//   dots: false,
-//   autoplay: true,
-//   autoplayTimeout: 3000,
-//   margin: 0,
-//   nav: false,
-//   touchDrag: true,
-//   mouseDrag: false,
-//   responsive: {
-//     0: {
-//       items: 1
-//     },
-//     600: {
-//       items: 1
-//     },
-//     1000: {
-//       items: 1
-//     }
-//   }
-// })
+$('.hero-carousel').owlCarousel({
+  loop: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  margin: 0,
+  nav: false,
+  touchDrag: true,
+  mouseDrag: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
 $('.music-carousel').owlCarousel({
   loop: true,
   dots: false,
